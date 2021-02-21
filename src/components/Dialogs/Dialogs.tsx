d1372f39059ebe88c14ca73ab702799f9ad49a6e
marmon179 <marmon179@gmail.com>
2021-02-21 17:12:11 +0300

    lesson 30 finish CSS 1.0

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -12,18 +12,21 @@ const Dialogs: React.FC<DialogPageType> = (props) => {
     return (
         <div className={styles.wrapper}>
             <div className={styles.wrapperImage}>
-                <img src="https://store.fcbarcelona.com/_ui/responsive/theme-blm/images/background-country.jpg" alt="Barca"/>
+                <img
+                    src="https://www.fcbarcelona.com/photo-resources/2020/11/02/d2c1d3ab-437b-400a-8df2-3e420b9f0799/imatge_avatars02.jpg?width=1024&height=448"
+                    alt="Barca"/>
             </div>
-            <div className={styles.dialogsWrapper}>
-                <div>
-                    {elementDialogs}
-                </div>
+            <div className={styles.wrapperWrapper}>
+                <div className={styles.dialogsWrapper}>
+                    <div>
+                        {elementDialogs}
+                    </div>
 
-                <div>
-                    {elementsMessages}
+                    <div>
+                        {elementsMessages}
+                    </div>
                 </div>
             </div>
-
         </div>
     )
 };
