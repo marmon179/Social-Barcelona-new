561119ac7ac8409a09c5d8430c89c009bd854fb6
marmon179 <marmon179@gmail.com>
2021-04-12 10:13:20 +0300

    lesson 55 no TS

--- src/components/Users/UsersContainer.tsx
+++ src/components/Users/UsersContainer.tsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import Users from './Users';
-import {followAC, InitialStateTypeUser, setUserAC, unfollowACC, UserType} from '../../redux/users-reducer';
+import {
+    followAC,
+    InitialStateTypeUser,
+    setCurrentPageAC, setTotalUsersCountAC,
+    setUserAC,
+    unfollowACC,
+    UserType
+} from '../../redux/users-reducer';
 import {AppStateType} from '../../redux/redux-store';
 import {Dispatch} from 'redux';
 
 type MapStatePropsType = {
     users: InitialStateTypeUser
+    pageSize: any
+    totalUsersCount: any
+    currentPage:any
 }
 
 type MapDispatchPropsType = {
     follow: (userId: string) => void
     unfollow: (userId: string) => void
     setUsers: (users: Array<UserType>) => void
+    setCurrentPage:(pageNumber:any) => void
+    setTotalUsersCount:(totalCount:any) => void
 }
 
 export type UsersPropsType = MapStatePropsType & MapDispatchPropsType
 
 const mapStateToProps = (state: AppStateType): MapStatePropsType => {
     return {
-        users: state.userPage
+        users: state.userPage,
+        pageSize: state.userPage.pageSize,
+        totalUsersCount: state.userPage.totalUsersCount,
+        currentPage: state.userPage.currentPage
     }
 }
 
@@ -33,6 +48,12 @@ const mapDispatchToProps = (dispatch: Dispatch): MapDispatchPropsType => {
         },
         setUsers: (users: Array<UserType>) => {
             dispatch(setUserAC(users))
+        },
+        setCurrentPage: (pageNumber) => {
+            dispatch(setCurrentPageAC(pageNumber))
+        },
+        setTotalUsersCount: (totalCount) => {
+            dispatch(setTotalUsersCountAC(totalCount))
         }
     }
 }
--- src/redux/users-reducer.tsx
+++ src/redux/users-reducer.tsx
@@ -1,5 +1,9 @@
 export type InitialStateTypeUser = {
     users: Array<UserType>
+    pageSize?: any
+    totalUsersCount?:any
+    currentPage?:any
+
 }
 export type UserType = {
     id: string
@@ -18,10 +22,15 @@ type ActionsTypes = {
     type: string
     userID: string
     users: any
+    currentPage:any
+    count:any
 }
 
 const initialState: InitialStateTypeUser = {
     users: [],
+    pageSize: 5,
+    totalUsersCount: 0,
+    currentPage:1
 }
 
 const usersReducer = (state: InitialStateTypeUser = initialState, action: ActionsTypes): InitialStateTypeUser => {
@@ -45,7 +54,13 @@ const usersReducer = (state: InitialStateTypeUser = initialState, action: Action
                 }))
             }
         case 'SET_USERS':
-            return {...state, users: [...state.users, ...action.users]}
+            return {...state, users:action.users}
+        case 'SET_CURRENT_PAGE': {
+            return {...state,currentPage:action.currentPage}
+        }
+        case 'SET_TOTAL_USERS_COUNT': {
+            return {...state,totalUsersCount: action.count}
+        }
         default:
             return state;
     }
@@ -55,5 +70,7 @@ const usersReducer = (state: InitialStateTypeUser = initialState, action: Action
 export const followAC = (userID: string) => ({type: 'FOLLOW', userID} as const)
 export const unfollowACC = (userID: string) => ({type: 'UNFOLLOW', userID} as const)
 export const setUserAC = (users: Array<UserType>) => ({type: 'SET_USERS', users} as const)
+export const setCurrentPageAC = (currentPage:any) => ({type: 'SET_CURRENT_PAGE', currentPage} as const)
+export const setTotalUsersCountAC = (totalUsersCount:any) => ({type: 'SET_TOTAL_USERS_COUNT', count: totalUsersCount} as const)
 
 export default usersReducer
